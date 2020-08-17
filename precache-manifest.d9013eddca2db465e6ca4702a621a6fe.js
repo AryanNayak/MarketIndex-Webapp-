a3a2bc6273d6f3f120c63b2753ff0d9045ebe89b
Aryan Nayak <aryannayakk@gmail.com>
2020-08-17 05:33:55 +0530

    Updates

---
+++ precache-manifest.d9013eddca2db465e6ca4702a621a6fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec98be53d8b279370c166b4cdd66b3e",
+    "url": "/MarketIndex-Webapp-/index.html"
+  },
+  {
+    "revision": "849e5899d465efefce79",
+    "url": "/MarketIndex-Webapp-/static/css/main.dbadf52e.chunk.css"
+  },
+  {
+    "revision": "eb41048bdf8d3af2babb",
+    "url": "/MarketIndex-Webapp-/static/js/2.b56a7487.chunk.js"
+  },
+  {
+    "revision": "ba747f7848ad46d7397281a03e1191df",
+    "url": "/MarketIndex-Webapp-/static/js/2.b56a7487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e5899d465efefce79",
+    "url": "/MarketIndex-Webapp-/static/js/main.845c834e.chunk.js"
+  },
+  {
+    "revision": "deb4aebf20160c444059",
+    "url": "/MarketIndex-Webapp-/static/js/runtime-main.8d087761.js"
+  },
+  {
+    "revision": "3f7e441e0aa24f4ef631465f91c2507d",
+    "url": "/MarketIndex-Webapp-/static/media/logo.3f7e441e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MarketIndex-Webapp-/static/media/logo.5d5d9eef.svg"
+  }
+]);
